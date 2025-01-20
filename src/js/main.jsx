ec90506923ce78ec82867873b70c2533334ad0ab
Aleperix <44621287+Aleperix@users.noreply.github.com>
2025-01-20 22:51:03 +0000

    repaso estados

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,10 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import Card from './components/Card';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Card cantidad={1}/>
   </React.StrictMode>,
 )
